a592c4e6770dfc292a6a18c4b389f5dad1408002
Daniele Tramontano <88477195+danieletramontano@users.noreply.github.com>
2025-08-01 11:59:12 +0200

    Lec 4

--- playground.hs
+++ playground.hs
@@ -1,11 +1,2 @@
--- import Data.Sequence (Seq(Empty))
-data IntList
-    = Empty
-    | Cons Int IntList
+import Data.Monoid (First)
 
-lengthIntList :: IntList -> Int
-lengthIntList Empty = 0
-lengthIntList (Cons _ xs) = 1 + lengthIntList xs
-
-showInt :: Int -> String
-showInt n = show n
